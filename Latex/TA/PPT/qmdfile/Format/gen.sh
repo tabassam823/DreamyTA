dee1480a5ac27dab8711af6817ac2e01f5031320
tabassam823 <kuranggaram@outlook.com>
2026-06-30 09:01:59 +0700

    PPT_1

---
+++ Latex/TA/PPT/qmdfile/Format/gen.sh
@@ -0,0 +1,18 @@
+for f in \
+02-outline \
+03-latar-belakang \
+04-rumusan-tujuan \
+05-dasar-teori \
+06-grup-lie-su \
+07-entanglement-measures \
+08-metodologi \
+09-parameterisasi-su \
+10-transformasi-su4 \
+11-hasil-klasifikasi \
+12-concurrence-three-tangle \
+13-interpretasi-su4 \
+14-kesimpulan \
+15-terima-kasih
+do
+    touch "sections/${f}.qmd"
+done
